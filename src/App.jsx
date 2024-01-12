901847f061e49fac774c722319d3ddd804e68448
rhbabu3@gmail.com <rhbabu3@gmail.com>
2024-01-13 00:15:16 +0600

    book finder project initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h2 className="text-5xl font-extrabold text-violet-500 text-center">
+                Book Finder
+            </h2>
+        </>
+    );
+}
+
+export default App;
